61cf1664780371f3b9306e982779f19f7df0880a
Randil12 <nguyen_david2003@outlook.fr>
2023-04-06 22:31:20 +0200

    CRUD Admin finish

---
+++ sat_libraries/admin.py
@@ -0,0 +1,59 @@
+import json
+import functions
+from datetime import datetime
+
+
+def DeleteOneSatelliteByAdmin(SatelliteID):
+    data = functions.get_satellites_data()
+    result = {"satellites" : ""}
+    for key, value in enumerate(data):
+        if (value['satID'] == SatelliteID):
+            data.pop(key)
+            print(key, value)
+    result['satellites'] = data
+
+    print(len(data) , result)
+    #print("OUI" , newdata , "OUIi" , len(data))
+    with open('../satellites.json', 'w') as file:
+        json.dump(result, file)
+
+
+
+
+def AddSatelliteByAdmin(name , launchDate , satAPO , satECC , satINC , satPER ,satLONG , satPOS):  # name , launchDate , satAPO , satECC , satINC , satPER ,satLONG , satPOS):
+    id = functions.get_maxId() +1
+    data = list(functions.get_satellites_data())
+
+    date = datetime.strptime(launchDate, "%Y-%m-%d")
+    result = {"satellites" : data}
+
+    print(len(result['satellites']))
+    new_satellite = {"satID": id, "satNAME": name + str(id), "launchDate": date.strftime("%Y-%m-%d"), "satAPO": satAPO, "satECC":satECC, "satINC": satINC, "satPER": satPER, "satLONG": satLONG, "satPOS": satPOS}
+
+    result['satellites'].append(new_satellite)
+    print(len(result['satellites']) , result)
+
+    with open('../satellites.json', 'w') as file:
+        json.dump(result, file)
+
+def UpdateSatelliteByAdmin(name , new_name , launchDate , satAPO , satECC , satINC , satPER ,satLONG , satPOS):
+    data = functions.get_satellites_data()
+    print(data)
+    date = datetime.strptime(launchDate, "%Y-%m-%d")
+    for key, value in enumerate(data):
+        if (value['satNAME'] == name):
+            value['satNAME'] = new_name
+            value['launchDate'] = date.strftime("%Y-%m-%d")
+            value['satAPO'] = satAPO
+            value['satECC'] = satECC
+            value['satINC'] = satINC
+            value['satPER'] = satPER
+            value['satLONG'] = satLONG
+            value['satPOS'] = satPOS
+
+    result = {"satellites": data}
+    with open('../satellites.json', 'w') as file:
+        json.dump(result, file)
+#AddSatelliteByAdmin("Sat", "2001-04-27", 1.7, 0.4, 163.9, 263.3, 268.3, 185.2)
+#DeleteOneSatelliteByAdmin(31)
+UpdateSatelliteByAdmin("Sat22" , "Sat" ,"2001-04-27", 1.7, 0.4, 163.9, 263.3, 268.3, 185.2)
--- sat_libraries/functions.py
+++ sat_libraries/functions.py
@@ -1,9 +1,18 @@
 import datetime
 import json
 
+
 # Recuperer les satellites par ordre aleatoire
 def get_satellites_data():
-    with open('satellites.json', 'r') as file:
+    with open('../satellites.json', 'r') as file:
         data = json.load(file)
     return data['satellites']
+
+
+def get_maxId():
+    data = get_satellites_data()
+    All_id = [e['satID'] for e in data]
+    return max(All_id)
+
+
 
